dc0f1147d66dcda57bc96c964ab8dbfb62334317
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-10-31 05:13:10 +0600

    feat: get todo endpoint

--- main.py
+++ main.py
@@ -26,6 +26,32 @@ class InputText(BaseModel):
     input_text: str
 
 
+class InputTodo(BaseModel):
+    topic: str
+
+
+@app.post('/get_todo')
+async def get_todo(input_data: InputTodo):
+    print("Getting suggestion for: ", input_data.topic)
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"You are a helpful AI assistant that can answer any questions about the given topic. Only answer to what's related to the topic, and don't go off-topic. Generate a list of to dos (max. 5) to achieve this goal: {input_data.topic}. Write shortly and be concise with points. Write only a sentence to each todo."
+                }
+            ]
+        )
+        response_message = response["choices"][0]["message"]["content"]
+
+        print(response_message)
+        return response_message
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.post("/get_graph")
 async def get_graph(input_data: InputText):
     print("Getting suggestion for: ", input_data.input_text)
